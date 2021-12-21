c8f08a2e6bc374bfc7087d7f14964cf8a4ea3b1e
mezhanglei <496623925@qq.com>
2021-12-21 22:28:15 +0800

    fix: webpack

--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -6,6 +6,8 @@ const FriendlyErrorsWebpackPlugin = require("friendly-errors-webpack-plugin");
 const TerserWebpackPlugin = require("terser-webpack-plugin");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
 const nodeExternals = require('webpack-node-externals');
+// 提取css webpack4推荐 在这之前用extracttext 作用是缓存css并解决样式闪动问题 因为只在编译阶段作用 所以不适用于热更新 但在生产环境无需配置热更新也没多大问题
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const configs = require('./configs.js');
 
 //  === webpack配置内容 === //
@@ -47,14 +49,26 @@ const webpackConfig = {
             {
                 test: /\.css$/,
                 use: [
-                    "style-loader",
+                    // 'style-loader',
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                        options: {
+                            publicPath: configs.assetsPath,
+                        },
+                    },
                     "css-loader",
                 ],
             },
             {
                 test: /\.less$/,
                 use: [
-                    "style-loader",
+                    // "style-loader",
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                        options: {
+                            publicPath: configs.assetsPath,
+                        },
+                    },
                     "css-loader",
                     "postcss-loader",
                     {
@@ -97,6 +111,9 @@ const webpackConfig = {
     },
     plugins: [
         new CleanWebpackPlugin(),
+        new MiniCssExtractPlugin({
+            filename: 'css/main.css'
+        }),
         new FriendlyErrorsWebpackPlugin(),
         new OptimizeCSSAssetsPlugin({
             assetNameRegExp: /\.css$/g,
